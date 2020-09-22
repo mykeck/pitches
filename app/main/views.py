9bb4b99f860e1a11f46222691172f4fb648a5788
MikeC <kiragumike@gmail.com>
2020-09-22 15:30:56 +0300

    imported bootsrap and wtf.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,abort
 from . import main
 from ..models import User,Post,Comment,Upvote,Downvote
 from flask_login import login_required, current_user
